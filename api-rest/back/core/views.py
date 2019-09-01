72bee5aaf76419ce4725cd4a2c4df0d88d0021ea
keane032 <keane032@gmail.com>
2019-09-01 10:58:35 -0300

    add swagger

--- api-rest/back/core/views.py
+++ api-rest/back/core/views.py
@@ -8,25 +8,82 @@ from rest_framework.response import Response
 import json
 
 class ProcessadorViewSet(viewsets.ModelViewSet):
+    """
+    create:
+        Cadastra um novo processador mae.
+
+    retrieve:
+        Retorna uma instancia de procecador pelo id.
+
+    list:
+        Retorna todas processadores cadastrados.
+
+    delete:
+        Remove uma processador existente pelo id.
+
+    """
     queryset = Processador.objects.all()
     serializer_class = ProcessadorSerializer
 
 class ClienteViewSet(viewsets.ModelViewSet):
+    """
+    create:
+        Cadastra um novo cliente.
+
+    retrieve:
+        Retorna uma instancia de cliente pelo id.
+
+    list:
+        Retorna todos clientes.
+
+    delete:
+        Remove uma cliente existente pelo id.
+
+    """
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
 class PlacaMaeViewSet(viewsets.ModelViewSet):
+    """
+    create:
+        Cadastra uma nova placa mae.
+
+    retrieve:
+        Retorna uma instancia de placa mae pelo id.
+
+    list:
+        Retorna todas placas .
+
+    delete:
+        Remove uma placa mae existente pelo id.
+
+    """
     queryset = PlacaMae.objects.all()
     serializer_class = PlacaMaeSerializer
 
 class MemoriaViewSet(viewsets.ModelViewSet):
+    """
+    create:
+        Cadastra uma nova memoria.
+
+
+    retrieve:
+        Retorna uma instancia de memoria pelo id.
+
+    list:
+        Retorna todas memorias.
+
+    delete:
+        Remove uma memoria existente pelo id.
+
+    """
     queryset = Memoria.objects.all()
     serializer_class = MemoriaSerializer   
 
 class PedidoViewSet(viewsets.ModelViewSet):
     """
     create:
-        Cria novo pedido.
+        Cadastra novo pedido.
         Cria um pedido passando os parametros:
             cliente(int),placamae(int),processador(int),placadevideo(string),memorias[int]
         
